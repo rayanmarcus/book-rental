ce95ec815e5d6c76cdec18ba731070c906831b36
Rayan Marcus <rayanmarcus@gmail.com>
2025-01-20 06:59:46 -0300

    chore fix lint

--- src/logic/types/books.ts
+++ src/logic/types/books.ts
@@ -1,15 +1,13 @@
-export type BookCollection = [
-  {
-    id: number;
-    img: string;
-    title: string;
-    author: string;
-    page_count: number;
-    language: string;
-    publisher: string;
-    publication_date: Date;
-    genre: string;
-    return_period: string;
-    price: number;
-  }
-];
+export type BookCollection = {
+  id: number
+  img: string
+  title: string
+  author: string
+  page_count: number
+  language: string
+  publisher: string
+  publication_date: Date
+  genre: string
+  return_period: string
+  price: number
+}
